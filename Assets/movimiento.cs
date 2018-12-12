7ccd91df33adbf3266471930cb709dbdba6c60df
Diego Mata <diego07mata@gmail.com>
2018-12-12 21:01:50 +0100

    Initial commit

---
+++ Assets/movimiento.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimiento : MonoBehaviour
+{
+    public KeyCode teclaD = KeyCode.D;
+    public KeyCode teclaA = KeyCode.A;
+    float movimientos = 1f;
+    // Use this for initialization
+    void Update()
+    {
+        if (Input.GetKeyDown(teclaD))
+        {
+            this.transform.Translate(new Vector3(0, movimientos* 7, 0));
+        }
+        
+    }
+        if (Input. ) 
+}
